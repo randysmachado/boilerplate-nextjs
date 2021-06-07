dd00505960f0955ec3792e357e2c767e739b88c9
randys <randys.paradise@gmail.com>
2021-06-07 17:07:20 -0300

    add README file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Starter Boilerplate
+
+Using:
+- [TypeScript](https://www.typescriptlang.org/)
+- [ReactJS](https://pt-br.reactjs.org/)
+- [NextJS](https://nextjs.org/)
+- [Styled Components](https://styled-components.com/)
+- [Jest](https://jestjs.io/pt-BR/)
+- [Storybook](https://storybook.js.org/)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,7 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate NextJS, TypeScript and Styled Components</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-192.png" />
+        <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple boilerplate starter with TypeScript, React, NextJS and Styled Components"
